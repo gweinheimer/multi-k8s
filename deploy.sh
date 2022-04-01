cd1311e91e252e3f7db226a8d2b1cc8ab9c58036
Greg Weinheimer <gweinhei@tibco.com>
2022-04-01 15:03:29 -0400

    added depolyment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t gweinheimer/multi-client:latest -t gweinheimer/multi-client:$SHA -f client/Dockerfile ./client
+docker build -t gweinheimer/multi-server:latest -t gweinheimer/multi-server:$SHA -f server/Dockerfile ./server
+docker build -t gweinheimer/multi-worker:latest -t gweinheimer/multi-worker:$SHA -f worker/Dockerfile ./worker
+# push images to dockerhub
+docker push gweinheimer/multi-client:latest  
+docker push gweinheimer/multi-server:latest  
+docker push gweinheimer/multi-worker:latest
+docker push gweinheimer/multi-client:$SHA  
+docker push gweinheimer/multi-server:$SHA  
+docker push gweinheimer/multi-worker:$SHA  
+#
+kubectl apply -f k8s
+#
+kubectl set image deployments/server server-deployment server=gweinheimer/multi-server:$SHA
+kubectl set image deployments/client client-deployment server=gweinheimer/multi-client:$SHA
+kubectl set image deployments/worker worker-deployment server=gweinheimer/multi-worker:$SHA
